65f766b507b6f3d9b68d0d8d2a9a940f303a5406
mvzurita10 <mikyzurita65@gmail.com>
2026-05-11 11:01:46 -0500

    Clase 11/05/2026

--- modulo2/shopapi/store/views/health.py
+++ modulo2/shopapi/store/views/health.py
@@ -7,4 +7,9 @@ from rest_framework.response import Response
 @api_view(['GET'])
 @permission_classes([AllowAny])
 def health_check(request):
-    return Response({'status': 'ok', 'version': '1.0'})
+    return Response(
+        {
+        'service': 'shopapi',
+        'status': 'ok',
+        'version': '1.0'
+        })
